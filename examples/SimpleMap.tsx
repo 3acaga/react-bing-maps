f849d48f8027f1e787ae63167bef21f40a81ae31
moon <3acaga12@gmail.com>
2019-07-10 16:21:28 +0300

    added simple example

---
+++ examples/SimpleMap.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactBingMap, { Pushpin, Polyline } from "@3acaga/react-bing-maps";
+
+const BingMap: React.FC = () => {
+  const start = {
+    latitude: 0,
+    longitude: 0
+  };
+
+  const end = {
+    latitude: 50,
+    longitude: 50
+  };
+
+  return (
+    <div>
+      <ReactBingMap apiKey="YOUR_API_KEY_HERE">
+        <Pushpin location={start} />
+
+        <Polyline path={[start, end]} />
+
+        <Pushpin location={end} />
+      </ReactBingMap>
+    </div>
+  );
+};
+
+export default BingMap;
